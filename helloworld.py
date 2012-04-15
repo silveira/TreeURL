c5884a3d99e052e0ad2d4d906b1fe1a1e1f92ebc
Silveira Neto <silveiraneto@gmail.com>
2012-04-15 00:59:28 -0400

    changed the directory strucutre, added jsPhyloSVG and RaphaelJS.

--- helloworld.py
+++ helloworld.py
@@ -19,6 +19,12 @@ class Greeting(db.Model):
   content = db.StringProperty(multiline=True)
   date = db.DateTimeProperty(auto_now_add=True)
 
+#class Greeting(db.Model):
+#  """Models an individual Guestbook entry with an author, content, and date."""
+#  author = db.UserProperty()
+#  content = db.StringProperty(multiline=True)
+#  date = db.DateTimeProperty(auto_now_add=True)
+
 
 def guestbook_key(guestbook_name=None):
   """Constructs a datastore key for a Guestbook entity with guestbook_name."""
